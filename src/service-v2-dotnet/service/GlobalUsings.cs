251b4ba3416fbc3f2d9e98fa4d09fb29cc08a0e2
mathias.herhold <Mathias.Herhold@actiware-development.com>
2022-01-29 15:59:52 +0100

    fix: add missing usings

--- src/service-v2-dotnet/service/GlobalUsings.cs
+++ src/service-v2-dotnet/service/GlobalUsings.cs
@@ -2,4 +2,5 @@ global using Serilog;
 global using Development.SDK.Logging.Extensions;
 global using Development.SDK.Module.Extensions;
 global using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+global using Microsoft.Extensions.Hosting.WindowsServices;
 global using Module.IOTemplate.Api.Info.Extensions;
