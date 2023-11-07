1d4cfa14971c8ce06851293c79c350fa8c26a715
Martin Schneider <martin.schneider@actiware-development.com>
2023-11-07 15:02:50 +0100

    refactor: remove service templates for go and nodejs

--- src/service-v2-go/models/service-stats.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type ServiceStats struct {
-	Product        string `json:"product"`
-	ProductName    string `json:"product_name"`
-	ProductVersion string `json:"product_version"`
-	FileVersion    string `json:"file_version"`
-	Status         bool   `json:"status"`
-}
