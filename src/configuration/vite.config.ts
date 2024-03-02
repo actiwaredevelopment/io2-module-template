1610950543eb897bebb0e64ff1c9f7ac5ed527b1
Mathias Herhold <mathias.herhold@actiware-development.com>
2024-03-02 11:23:09 +0100

    fix: wrong default ports and add missing icon information

--- src/configuration/vite.config.ts
+++ src/configuration/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-    const port = parseInt(process.env.PORT ?? '31234');
+    const port = parseInt(process.env.PORT ?? '30100');
 
     return {
         build: {
@@ -15,3 +15,4 @@ export default defineConfig(() => {
         }
     };
 });
+
