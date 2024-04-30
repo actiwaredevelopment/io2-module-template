0595631a2ad85961c9e37eb57df88285727504dd
Martin Schneider <martin.schneider@actiware-development.com>
2024-04-30 18:29:30 +0200

    refactor: update icons to Font Awsome

--- src/configuration/src/custom-icons.tsx
+++ src/configuration/src/custom-icons.tsx
@@ -1,24 +1,67 @@
-import { registerIcons } from '@fluentui/react';
+import { getTheme, registerIcons } from '@fluentui/react';
+import {
+    faPlus,
+    faTrash,
+    faPen,
+    faSave,
+    faPlay,
+    faCopy,
+    faPaste,
+    faClone,
+    faCircleQuestion,
+    faExclamationTriangle,
+    faSync,
+    faListAlt,
+    faCircleTrash,
+    faArrowDown,
+    faArrowUp,
+    faBookOpenReader,
+    faPenLine,
+    faSpinner,
+    faEllipsisVertical,
+    faCircleExclamation,
+    faTriangleExclamation,
+    faUserPlus
+} from '@fortawesome/pro-light-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const theme = getTheme();
 
 export function initializeCustomIcons() {
     registerIcons({
         icons: {
-            'fa-arrow-down': <i className='fa-light fa-arrow-down' />,
-            'fa-arrow-up': <i className='fa-light fa-arrow-up' />,
-            'fa-circle-exclamation': <i className='fa-light fa-circle-exclamation' />,
-            'fa-circle-question': <i className='fa-light fa-circle-question' />,
-            'fa-clone': <i className='fa-light fa-clone' />,
-            'fa-copy': <i className='fa-light fa-copy' />,
-            'fa-list-alt': <i className='fa-light fa-list-alt' />,
-            'fa-paste': <i className='fa-light fa-paste' />,
-            'fa-pen': <i className='fa-light fa-pen' />,
-            'fa-play': <i className='fa-light fa-play' />,
-            'fa-plus': <i className='fa-light fa-plus' />,
-            'fa-save': <i className='fa-light fa-save' />,
-            'fa-spinner': <i className='fa-light fa-spinner fa-spin' />,
-            'fa-sync': <i className='fa-light fa-sync' />,
-            'fa-trash': <i className='fa-light fa-trash' />
+            'fa-spinner': (
+                <FontAwesomeIcon
+                    icon={faSpinner}
+                    spin
+                />
+            ),
+            'fa-sync': <FontAwesomeIcon icon={faSync} />,
+            'fa-save': <FontAwesomeIcon icon={faSave} />,
+            'fa-plus': <FontAwesomeIcon icon={faPlus} />,
+            'fa-play': <FontAwesomeIcon icon={faPlay} />,
+            'fa-trash': <FontAwesomeIcon icon={faTrash} />,
+            'fa-circle-trash': <FontAwesomeIcon icon={faCircleTrash} />,
+            'fa-clone': <FontAwesomeIcon icon={faClone} />,
+            'fa-pen': <FontAwesomeIcon icon={faPen} />,
+            'fa-copy': <FontAwesomeIcon icon={faCopy} />,
+            'fa-paste': <FontAwesomeIcon icon={faPaste} />,
+            'fa-exclamation-triangle': <FontAwesomeIcon icon={faExclamationTriangle} />,
+            'fa-circle-question': <FontAwesomeIcon icon={faCircleQuestion} />,
+            'fa-circle-exclamation': <FontAwesomeIcon icon={faCircleExclamation} />,
+            'fa-list-alt': <FontAwesomeIcon icon={faListAlt} />,
+            'fa-database-reader': <FontAwesomeIcon icon={faBookOpenReader} />,
+            'fa-database-writer': <FontAwesomeIcon icon={faPenLine} />,
+            'fa-arrow-up': <FontAwesomeIcon icon={faArrowUp} />,
+            'fa-arrow-down': <FontAwesomeIcon icon={faArrowDown} />,
+            FarEllipsisVertical: <FontAwesomeIcon icon={faEllipsisVertical} />,
+            'fal-triangle-exclamation-colored': (
+                <FontAwesomeIcon
+                    color={theme.semanticColors.errorIcon}
+                    icon={faTriangleExclamation}
+                />
+            ),
+            'fa-user-plus': <FontAwesomeIcon icon={faUserPlus} />
         }
     });
 }
-
